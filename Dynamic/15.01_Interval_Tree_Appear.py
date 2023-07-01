a0ad4a8002bccaf200174d87b60f81f1bb25c540
Maks <mzawislak@student.agh.edu.pl>
2023-07-01 16:29:16 +0200

    lecture 15

---
+++ Dynamic/15.01_Interval_Tree_Appear.py
@@ -0,0 +1,119 @@
+# appearing of intervals on given point
+class IntervalTree:
+    def __init__(self, value: int = None, left=None, right=None, parent=None, span=None):
+
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+        self.intervals = []
+        if span is None:
+            span = [-1 * float('inf'), float('inf')]
+        self.span = span
+
+    def in_span(self, given_interval: list) -> bool:
+        if self.span[0] >= given_interval[0] and self.span[1] <= given_interval[1]:
+            return True
+        return False
+
+    def common_part(self, given_interval: list) -> bool:
+        if given_interval[1] < self.span[0] or given_interval[0] > self.span[1]:
+            return False
+        return True
+
+
+def build_tree(intervals: list) -> IntervalTree:
+    points = []
+
+    for u, v in intervals:
+        if u not in points:
+            points.append(u)
+        if v not in points:
+            points.append(v)
+
+    points.sort(key=lambda x: x)
+    print(points)
+
+    def middle_element(i=0, j=len(points) - 1) -> int:
+        return (i + j) // 2
+
+    def make_tree(prev_node: IntervalTree = None, left_end: int = 0, right_end: int = len(points) - 1,
+                  left_span_end: int = -1 * float('inf'), right_span_end: int = float('inf'),
+                  last: bool = False) -> IntervalTree:
+
+        if last is True:
+            node = IntervalTree()
+            node.parent = prev_node
+            node.span = [left_span_end, right_span_end]
+            return node
+
+        mid_elem = middle_element(left_end, right_end)
+        node = IntervalTree()
+        node.parent = prev_node
+        node.value = points[mid_elem]
+        node.span = [left_span_end, right_span_end]
+
+        if left_end == right_end:
+            last = True
+
+        node.left = make_tree(node, left_end, mid_elem - 1, left_span_end, points[mid_elem], last)
+        node.right = make_tree(node, mid_elem + 1, right_end, points[mid_elem], right_span_end, last)
+        return node
+
+    root = make_tree()
+
+    return root
+
+
+def print_tree(node: IntervalTree):
+    if node.left is not None:
+        print_tree(node.left)
+    print(f"Node value is: {node.value}, node span is: {node.span}, and found intervals are: {node.intervals}")
+    if node.right is not None:
+        print_tree(node.right)
+
+
+def solve_task(root: IntervalTree, given_intervals: list) -> None:
+    def put_interval(node: IntervalTree, interval: list):
+        if node.in_span(interval):
+            node.intervals.append(interval)
+            return
+
+        if node.left is not None:
+            if node.left.common_part(interval):
+                put_interval(node.left, interval)
+        if node.right is not None:
+            if node.right.common_part(interval):
+                put_interval(node.right, interval)
+
+    for interval in given_intervals:
+        put_interval(root, interval)
+
+    print_tree(root)
+
+
+def check_intervals_point(root: IntervalTree, value: int) -> int:
+    print("Solution:")
+    in_intervals = []
+    def go_down(node: IntervalTree):
+        nonlocal in_intervals
+
+        for interval in node.intervals:
+            if interval not in in_intervals:
+                in_intervals.append(interval)
+
+        print(f"Node value: {node.value}, node intervals: {node.intervals}")
+        if node.left is not None and node.value >= value:
+            go_down(node.left)
+        if node.right is not None and node.value <= value:
+            go_down(node.right)
+
+    go_down(root)
+
+    return len(in_intervals)
+
+
+given_intervals = [[0, 10], [5, 20], [7, 12], [10, 15]]
+tree_root = build_tree(given_intervals)
+solve_task(tree_root, given_intervals)
+print(check_intervals_point(tree_root, 13))
---
+++ Dynamic/15.02_Interval_Tree_Sum.py
@@ -0,0 +1,97 @@
+import math
+
+
+class IntervalTree:
+    def __init__(self, value: int, start: int, end: int, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+        self.start = start
+        self.end = end
+
+    def common_part(self, given_start, given_end) -> bool:
+        if given_end < self.start or given_start > self.end:
+            return False
+        return True
+
+    def perfect_match(self, given_start, given_end) -> bool:
+        if given_start == self.start and given_end == self.end:
+            return True
+        return False
+
+
+def build_tree(nums: list) -> (IntervalTree, list):
+    n = len(nums)
+    print(math.sqrt(n) + 1)
+    to_add = int(2 ** (math.sqrt(n) + 1) - n)
+    zeros = [0 for _ in range(to_add)]
+    nums += zeros
+    print(len(nums))
+    print(nums)
+    leafs = []
+    for i, value in enumerate(nums):
+        tmp = IntervalTree(value, i, i)
+        leafs.append(tmp)
+
+    level = leafs.copy()
+
+    while len(level) != 1:
+        new_level = []
+
+        for i in range(0, len(level), 2):
+            left_child = level[i]
+            right_child = level[i + 1]
+
+            tmp = IntervalTree(left_child.value + right_child.value, left_child.start, right_child.end, left_child,
+                               right_child)
+            left_child.parent = tmp
+            right_child.parent = tmp
+            new_level.append(tmp)
+        level = new_level
+
+    root = level.pop()
+    return root, leafs
+
+
+def tree_update(index: int, new_value: int, leafs: list) -> None:
+    difference = new_value - leafs[index].value
+
+    def change_value(node: IntervalTree):
+        nonlocal difference
+        node.value += difference
+        if node.parent is not None:
+            change_value(node.parent)
+
+    change_value(leafs[index])
+
+
+def tree_sum(root: IntervalTree, i: int, j: int) -> int:
+    count = 0
+
+    def sum_up(node: IntervalTree, i: int, j: int):
+        nonlocal count
+        if not node.common_part(i, j):
+            return
+
+        if node.perfect_match(i, j):
+            count += node.value
+            return
+
+        sum_up(node.left, i, min(node.left.end,j))
+        sum_up(node.right, max(node.right.start,i), j)
+
+    sum_up(root, i, j)
+    print(f'Found sum: {count}')
+    return count
+
+
+numbers = [0, 7, 5, 3, 1, 8, 6, 12, 3]
+tree_root, tree_leafs = build_tree(numbers)
+print(tree_root.value)
+tree_update(3,6,tree_leafs)
+print(tree_root.value)
+
+tree_sum(tree_root,1,6)
+
+
